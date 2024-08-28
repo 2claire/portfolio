31932dbdf4ad8e5bb84b2c46a05545fec4795bd1
2claire <njorogeclaire26@gmail.com>
2024-08-28 12:49:05 +0300

    messaage

--- portfolio/src/components/Hero.vue
+++ portfolio/src/components/Hero.vue
@@ -2,7 +2,18 @@
   <section>
     <div class="flex flex-col sm:flex-row justify-center items-center gap-10 py-3 px-10">
     <div><img src="../assets/media/indian.JPG" class="h-[450px] w-[450px] rounded-lg" /></div>
-    <div class="border-2 h-[450px] w-[450px] rounded-lg"></div>
+    <div class="border-2 h-[450px] w-[450px] rounded-lg">
+      <h1 class="text-2xl font-bold py-4 px-6">Personal Description:</h1>
+      <p class="text-xl ">
+        I am a passionate and versatile person.
+        My work is driven by a commitment, passion, discipline,scalability, 
+        and a keen eye for design.
+        I am continuously learning and experimenting with new technologies, 
+        striving to stay at the forefront of the industry.
+        My goal is to build meaningful, impactful software that not only meets functional 
+        requirements but also delights users with exceptional design and performance.
+      </p>
+    </div>
     </div>
   <hr/>
   </section>
